c37830ebcf589d91b6ce6eb81d1be5e2a6c470f8
Jordan <Jordforb@gmail.com>
2022-05-12 09:50:50 -0400

    added basic routes

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-from 
+from flask import Flask, redirect, url_for,render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "hello"
+
+if __name__=="__main__":
+    app.run()

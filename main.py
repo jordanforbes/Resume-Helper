a7973be90cda7b0c04b15004e88ed5e3c23a218c
Jordan <Jordforb@gmail.com>
2022-05-12 10:00:38 -0400

    added get/post methods

--- main.py
+++ main.py
@@ -4,7 +4,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "hello"
+    return render_template("index.html")
+
+@app.route('/login', methods= ["POST", "GET"])
+def login():
+    if request.method =="POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr= user))
+    return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 if __name__=="__main__":
-    app.run()
+    app.run(debut=True)
